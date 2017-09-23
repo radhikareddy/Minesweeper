6681ef2f7bb01ced7b87bd32593cd596f2961f59
Radhika Nooka <nookaradhikareddy@gmail.com>
2017-09-23 00:06:19 -0700

    first commit

---
+++ src/minesweeper.js
@@ -0,0 +1,13 @@
+const blankLine = ' |   | ';
+const guessLine = '1|   | ';
+const bombLine  = ' | B | ';
+//first log
+console.log("This is what an empty board would look like:");
+console.log(`${blankLine}`);
+console.log(`${blankLine}`);
+console.log(`${blankLine}`);
+//second log
+console.log("This is what a board with a guess and a bomb on it would look like:")
+console.log(`${guessLine}`);
+console.log(`${bombLine}`);
+console.log(`${blankLine}`);
