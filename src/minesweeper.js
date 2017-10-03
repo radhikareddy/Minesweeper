1eb9c988657bca90fbd59707ae5a14cf71e54ccc
Radhika Nooka <nookaradhikareddy@gmail.com>
2017-10-02 17:39:47 -0700

    third commit

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,43 @@
-let board = [
-  [' ',' ',' '],
-  [' ',' ',' '],
-  [' ',' ',' ']
-];
-const printBoard = (board) => {
-  console.log("Current Board:");
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for(let i=0; i< numberOfRows; i++){
+    let row =[];
+    for(let j=0;j< numberOfColumns; j++){
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
-//empty board
-printBoard(board);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for(let rowsIndex=0; rowsIndex< numberOfRows; rowsIndex++){
+    let row =[];
+    for(let columnsIndex=0;columnsIndex< numberOfColumns; columnsIndex++){
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while(numberOfBombsPlaced != numberOfBombs){
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    //TBD loop has the potential to place bombs on top of already existing bombs
+  }
+  return board;
+}
+const printBoard = (board) => {
+  let result = board.map(row => row.join(' | ')).join('\n')
+ console.log( result );
+ //console.log("calling printBoard");
+};
 
-//setting values on board
-board[0][1] = 1;
-board[2][2] = 'B';
-printBoard(board);
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard)
